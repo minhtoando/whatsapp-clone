2d274eaa42731d40b8cfc683aaa228e2b9775cb4
minhtoando <65742565+minhtoando@users.noreply.github.com>
2020-09-06 23:47:28 -0400

    created NewConversationsModal component

--- src/components/NewConversationsModal.js
+++ src/components/NewConversationsModal.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Modal, Form, Button } from 'react-bootstrap'
+import { useContacts } from '../contexts/ContactsProvider' 
+import { useConversations } from '../contexts/ConversationsProvider'
 
-export default function NewConversationsModal() {
+export default function NewConversationsModal({ closeModal }) {
+    const [selectedContactsIds, setSelectedContactsIds] = useState([])
+    const { contacts } = useContacts()
+    const { createConversation } = useConversations()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        createConversation(selectedContactsIds)
+        closeModal()
+    }
+
+    function handleCheckBoxChange(contactId){
+        setSelectedContactsIds(prevSelectedContactsIds => {
+            if (prevSelectedContactsIds.includes(contactId)){
+                return prevSelectedContactsIds.filter(prevId => {
+                    return contactId !== prevId
+                })
+            } else {
+                return[...prevSelectedContactsIds, contactId]
+            }
+        })
+    }
+
     return (
-        <div>
-            
-        </div>
+        <>
+            <Modal.Header closeButton>Create Conversation</Modal.Header>
+            <Modal.Body>
+                <Form onSubmit={handleSubmit}>
+                    {contacts.map(contact => (
+                        <Form.Group controlId={contact.id} key={contact.id}>
+                            <Form.Check
+                                type="checkbox"
+                                value={selectedContactsIds.includes(contact.id)}
+                                label={contact.name}
+                                onChange={() => handleCheckBoxChange(contact.id)}
+                            />
+                        </Form.Group>
+                    ))}
+                    <Button type="submit">Create</Button>
+                </Form>
+            </Modal.Body>
+        </>
     )
 }
